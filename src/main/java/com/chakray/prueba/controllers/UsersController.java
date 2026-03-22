63ac10300a66173d4779fd3ed3a0c567fd81d4ea
satoxehclim <xehclim1@gmail.com>
2026-03-21 23:34:19 -0600

    Validacion y encriptado de saveUser

--- src/main/java/com/chakray/prueba/controllers/UsersController.java
+++ src/main/java/com/chakray/prueba/controllers/UsersController.java
@@ -5,6 +5,8 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,6 +31,8 @@ public class UsersController {
     UsersService usersService;
 
     Encrypt encrypt = new Encrypt();
+    private final String regexTaxId = "^[A-ZÑ&]{3,4}[0-9]{6}[A-Z0-9]{3}$";
+    private final String regexPhone = "^(\\+[0-9]{1,3} )?[0-9]{2} [0-9]{3} [0-9]{3} [0-9]{2}$";
 
     @GetMapping()
     public ArrayList<UsersModel> getUsers(@RequestParam(name ="sortedBy", required = false) String sortedBy){
@@ -48,11 +52,25 @@ public class UsersController {
         return users;
     }
 
+    //TODO: Implementar endpoint para filtrar usuarios con los parametros[email|id|name|phone|tax_id|created_at]+[co|eq|sw|ew]+[value], donde co es contains, eq es equals, sw es starts with y ew es ends with
+
     @PostMapping()
     public String saveUser(@RequestBody UsersModel user){
 
-        if (user.getEmail() == null || user.getName() == null || user.getPhone() == null || user.getPassword() == null || user.getTax_id() == null) {
-            return "{\"message\": \"missing required fields\"}";
+        if (user.getEmail() == null || user.getName() == null || user.getPhone() == null || user.getPassword() == null || user.getTax_id() == null || user.getAddresses() == null) {
+            return "{\"message\": \"missing required fields, the following fields are required: email, name, phone, password, tax_id and addresses object\"}";
+        }
+
+        Pattern patternTaxId = Pattern.compile(regexTaxId);
+        Matcher matcherTaxId = patternTaxId.matcher(user.getTax_id());
+        while (!matcherTaxId.find()) {
+            return "{\"message\": \"invalid tax_id format, it should be like ABCD981220KM7\"}";
+        }
+
+        Pattern patternPhone = Pattern.compile(regexPhone);
+        Matcher matcherPhone = patternPhone.matcher(user.getPhone());
+        while (!matcherPhone.find()) {
+            return "{\"message\": \"invalid phone format, it should be like +1 55 555 555 55 or +12 55 555 555 55 or +123 55 555 555 55 or 55 555 555 55\"}";
         }
 
         ZoneId zoneId = ZoneId.of("GMT+3");
@@ -62,7 +80,11 @@ public class UsersController {
         
         user.setPassword(encrypt.encryptAES256(user.getPassword()));
 
-        return this.usersService.saveUser(user).toString();
+        try {
+            return this.usersService.saveUser(user).toString();
+        } catch (Exception e) {
+            return "{\"message\": \"error saving user, review the data\"}";
+        }
     }
 
     @DeleteMapping( path = "/{id}")
@@ -74,5 +96,7 @@ public class UsersController {
             return "{\"message\": \"could not delete user\"}";
         }
     }
+
+    //TODO: Implementar endpoint para actualizar usuario
 
 }
--- src/main/java/com/chakray/prueba/repositories/UsersRepository.java
+++ src/main/java/com/chakray/prueba/repositories/UsersRepository.java
@@ -1,5 +1,7 @@
 package com.chakray.prueba.repositories;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,32 @@ import com.chakray.prueba.models.UsersModel;
 @Repository
 public interface  UsersRepository extends CrudRepository<UsersModel, Long> {
 
+    // Nota para los desarrolladores: Separe los metodos para tener mejor legiibilidad
+    // Estos repositoriios son para el endpoint equals
+    public abstract ArrayList<UsersModel> findByEmail(String email);
+    public abstract ArrayList<UsersModel> findByName(String name);
+    public abstract ArrayList<UsersModel> findByPhone(String phone);
+    public abstract ArrayList<UsersModel> findByTax_id(String tax_id);
+    public abstract ArrayList<UsersModel> findByCreated_at(String created_at);
+
+    // Estos repositorios son para el endpoint contains
+    public abstract ArrayList<UsersModel> findByEmailContains(String email);
+    public abstract ArrayList<UsersModel> findByNameContains(String name);
+    public abstract ArrayList<UsersModel> findByPhoneContains(String phone);
+    public abstract ArrayList<UsersModel> findByTax_idContains(String tax_id);
+    public abstract ArrayList<UsersModel> findByCreated_atContains(String created_at);
+
+    // Estos repositorios son para el endpoint starts with
+    public abstract ArrayList<UsersModel> findByEmailStartsWith(String email);
+    public abstract ArrayList<UsersModel> findByNameStartsWith(String name);
+    public abstract ArrayList<UsersModel> findByPhoneStartsWith(String phone);
+    public abstract ArrayList<UsersModel> findByTax_idStartsWith(String tax_id);
+    public abstract ArrayList<UsersModel> findByCreated_atStartsWith(String created_at);
+
+    // Estos repositorios son para el endpoint ends with
+    public abstract ArrayList<UsersModel> findByEmailEndsWith(String email);
+    public abstract ArrayList<UsersModel> findByNameEndsWith(String name);
+    public abstract ArrayList<UsersModel> findByPhoneEndsWith(String phone);
+    public abstract ArrayList<UsersModel> findByTax_idEndsWith(String tax_id);
+    public abstract ArrayList<UsersModel> findByCreated_atEndsWith(String created_at);
 }
